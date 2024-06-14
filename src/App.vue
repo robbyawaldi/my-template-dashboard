2798ecb47c65d8d18ff131d1a2695e5e532316c6
Robby Awaldi <47056508+robbyawaldi@users.noreply.github.com>
2024-06-14 09:54:24 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+const checked = true;
+</script>
+
+<template>
+  <div class="p-6 flex gap-4 flex-wrap">
+    <a-button type="primary" class="my-button">Primary Button</a-button>
+    <a-checkbox class="my-checkbox" v-model:checked="checked"></a-checkbox>
+    <a-select
+      class="my-select"
+      ref="select"
+      v-model:value="value1"
+      style="width: 120px"
+      @focus="focus"
+      @change="handleChange"
+    >
+      <a-select-option value="jack">Jack</a-select-option>
+      <a-select-option value="lucy">Lucy</a-select-option>
+      <a-select-option value="disabled" disabled>Disabled</a-select-option>
+      <a-select-option value="Yiminghe">yiminghe</a-select-option>
+    </a-select>
+  </div>
+</template>
